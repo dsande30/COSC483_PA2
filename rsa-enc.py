29355c1360ad69a9aaab8f603047489a7f22ec09
Unknown <dsande30@vols.utk.edu>
2017-11-10 07:07:46 -0500

    Ugh

--- rsa-enc.py
+++ rsa-enc.py
@@ -52,88 +52,41 @@ def readInput(inputFile):
 
 
 def pad(message, r):
+    print "Bear with me Schuchard"
     M = b'\x00' + b'\x02'
-
-    print "Seed:", random.seed()
-
-    #ensure always 20 bits
-    randBits = 0
-    while randBits.bit_length() != 20:
-        randBits = random.getrandbits(r)
-
-    #need direct copy of bits
-    #check = randBits.bit_length()
-
-    #seperate into chunks
-    bitBlocks = []
-
-    check = hex(randBits)
-
-    print "Check Hex stuff: %s" % check.strip('L')
-
-    """
-    while len(check) > 0:
-        slicelen = min(len(check), 8)
-        bitBlocks.append(check[0:slicelen])
-        check = check[slicelen:]
-    """
-
-    for n in bitBlocks:
-        if n == b'\x00':
-            while n == b'\x00':
-                n = random.getrandbits(8)
-
-    x = random.getrandbits(8)
-    print "X: %s" % str(x).encode('hex')
-
-'''
-def pad(message, r):
-    paddedM = b'\x00' + b'\x02'
-    print("r: ", r)
-    #print("Length of r: ", len(r))
-    message = message.strip()
-    if len(message) < (r-24):
-        message += "0" * ((r-24)-len(message))
-    print("Message: %s" % message)
-    #print("Message Length: ", len(message))
-    #test = 0
-    #while test == 0:
-    test = 1
-    randBits = random.getrandbits(r)
-    print "randBits before encoding: %s" % randBits
-    #randBits = randBits.encode('utf-8')
-    #print "randBits after encoding: %s" % randBits
-    bitBlocks = []
-    check = str(randBits)[:]
-    while len(check) > 0:
-        slicelen = min(len(check), 8)
-        bitBlocks.append(check[0:slicelen])
-        check = check[slicelen:]
-    #print("randBits: %s" % randBits)
-    #print("Length of randBits: ", len(randBits))
-    print("bitBlocks: ", bitBlocks)
-    #print("bitBlocks Length: ", len(bitBlocks))
-    for n in bitBlocks:
-        if n == b'\x00':
-            while n == b'\x00':
-                n = str(random.getrandbits(8))
-            #test = 0
-    #randBits = binascii.hexlify(randBits)
-    print("Hex randBits: 0x%s" % randBits)
-    paddedM += format(int(randBits), '02x') + b'\x00' + binascii.hexlify(message)
-    return paddedM
-'''
+    test = 0
+    while test == 0:
+        test = 1
+        rand = random.getrandbits(r)
+        rand = str(rand)
+        randlength = 0
+        for i in range(0, len(rand)):
+            if rand[i] == '0':
+                test = 0
+        for i in range(0, len(rand)):
+            randlength += int(rand[i]).bit_length()
+        if randlength != r:
+            test = 0
+    print "Rand: %s" % rand
+    print randlength
+    M += rand + b'\x00'
+    testlength = 0
+    for i in range(0, len(M)):
+        testlength += int(M[i]).bit_length()
+    bitlength = randlength + 4
+    print "Testlength: %d" % testlength
+    print "Bitlength: %d" % bitlength
+    M += message.strip()
+    print "%s" % M
+    print "%s" % M[:-1]
 
 def main():
     args = getFlags()
     contents = readKey(args.keyFile)
     message = readInput(args.inputFile)
-    #paddedM = pad(message, int(contents[0]) / 2) #NOTE:Doesn't work
-
-    #NOTE: Arbitrary padded message for now
-    paddedM = 7654321
-    c = Encrypt(paddedM, contents)
-    writeOutput(args.outputFile, c)
+    paddedM = pad(message, int(contents[0]) / 2)
+    #c = Encrypt(paddedM, contents)
+    #writeOutput(args.outputFile, c)
 
 if __name__ == "__main__":
 	main()
