3cdde86312d60f9fb4c4710119bc9105e32b41e6
Unknown <dsande30@vols.utk.edu>
2017-11-10 00:50:19 -0500

    Corrected value generator

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -4,6 +4,7 @@ from Crypto.Util import number
 import fractions
 
 
+
 def getFlags():
     #parse command line args
     parser = argparse.ArgumentParser()
@@ -18,26 +19,22 @@ def variableGenerator(numBits):
 
     #to make p and q
     numBits = numBits
-    print "N: %d\n" % numBits
+    print "N: %d" % numBits
     p = 0
     q = 0
     l = (numBits / 2)
-    print "L: %d\n" % l
+    print "L: %d" % l
     while(p ==0 and q == 0):
         p = number.getPrime(l)
         q = number.getPrime(l)
-        print "p",sys.getsizeof(p)
-        print "q",sys.getsizeof(q)
-        if len(str(p)) != len(str(q)):
-            p = int(p)
-            q = int(q)
+        print "p bitlength %d" % p.bit_length()
+        print "q bitlength %d" % q.bit_length()
+        #confirm lengths are the same, not the values
+        if p.bit_length() != q.bit_length() or p == q:
             p = 0
             q = 0
-    print "Len P: %d" % len(str(p))
-    print "Len Q: %d" % len(str(q))
-    print "Len N: %d" % len(str(int(p) * int(q)))
-    print "P: %d\n" % int(p)
-    print "Q: %d\n" % int(q)
+    print "P: %d" % int(p)
+    print "Q: %d" % int(q)
 
     p = int(p)
     q = int(q)
@@ -45,8 +42,26 @@ def variableGenerator(numBits):
     N = p * q
     order = (p-1)*(q-1)
 
-    print "N: %d\n" % N
-    print "Order: %d\n" % order
+    print "N: %d" % N
+    print "Order: %d" % order
+
+    #calculate e: coprime to order
+    #NOTE: I am not confident this works.
+    ePrimes = [3,5,7,17,257, 65537]
+    booly = 0
+    for x in ePrimes:
+        if(fractions.gcd(order, x) == 1):
+            booly = 1
+            print "e: %d" % x
+            e = x
+            break
+    if booly == 0:
+        sys.exit("No coprime")
+
+    #d is inverse of e mod order
+    d = number.inverse(e, order)
+    print "d: %d" % d
+
 
 def writeFiles(args):
     pub = open(args.publicFile, 'w')
